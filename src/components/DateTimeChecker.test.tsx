78d9e863cdcdbb5dc256158b56d862c0721ea148
lequanghieu2504 <lequanghieu2504@gmail.com>
2025-09-25 16:25:19 +0700

    succes

--- src/components/DateTimeChecker.test.tsx
+++ src/components/DateTimeChecker.test.tsx
@@ -9,7 +9,7 @@ describe("validateDate", () => {
   });
 
   it("should return an error message for an empty string", () => {
-    expect(validateDate("11111")).toBe("Date field cannot be empty.");
+    expect(validateDate("")).toBe("Date field cannot be empty.");
   });
 
   it("should return an error for year less than 1000", () => {
